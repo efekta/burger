5ad85a2d71938c9dc35c20f19b5797736c654832
efekta <efekta9@gmail.com>
2017-05-25 20:17:02 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -194,7 +194,7 @@ $(function () {
 			myCollection = new ymaps.GeoObjectCollection({}, {
 				draggable: false,
 				iconLayout: 'default#image',
-				iconImageHref: '../img/icons/map-marker.svg',
+				iconImageHref: 'img/icons/map-marker.svg',
 				iconImageSize: [46, 57],
 				iconImageOffset: [-26, -52]
 			});
